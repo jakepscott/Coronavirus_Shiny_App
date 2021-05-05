3c3d4e3df4ec3fc2dc51c34d42a29c9b287f6630
jakepscott <jakepscott16@gmail.com>
2021-05-04 23:09:56 -0400

    Add loading bar

--- Map_Test/app.R
+++ Map_Test/app.R
@@ -65,73 +65,83 @@ ui <- fluidPage(
 server <- function(input, output) {
     # Clean and Wrangle Data --------------------------------------------------
     #Add state names and pop data to NYT Data
-    US_Data <- reactive(US_Data_Raw %>% 
-        #keep only selected state
-        filter(state==input$statenameformap,
-               date>=input$map_date-7 & date<=input$map_date,
-               county!="Unknown",
-               !str_detect(county,"Aleutians West")) %>% # This filters the data to just be the seven days leading up to the date of interest, so we can get the 7 day rolling average
-        #Add state names
-    left_join(State_Names, by=c("state")) %>% 
-        #Add state pops
-        left_join(state_pop_clean,by="state") %>%
-        #Add county pops
-        left_join(county_pop_clean, by=c("state","county")) %>% 
-        #Rename some columns
-        rename("County"=county,"State"=state, 
-               "Date"=date, "Cases"=cases,"Deaths"=deaths) %>% 
-        #Grab the county fips and add NYC pop
-        mutate(county_fips=substr(fips,3,5),
-               County_Population=ifelse(County=="New York City", 8419000, County_Population)) %>% 
-    #Generate new variables
-        arrange(County,Date) %>% 
-        group_by(fips) %>% 
-        mutate(Cases_Per_Million=Cases/County_Population*1000000,
-               Deaths_Per_Million=Deaths/County_Population*1000000,
-               
-               New_Cases=Cases-lag(Cases), ##New cases is today's cases minus yesterdays
-               New_Cases=ifelse(New_Cases<0,0,New_Cases), # Set it to zero if it is negative
-               New_Cases_Average=rollmean(New_Cases,k = 7,fill = NA, align = "right"),
-               New_Cases_Per_Million=(New_Cases/County_Population)*1000000,
-               New_Cases_Per_Million_Average=(New_Cases_Average/County_Population)*1000000,
-               
-               New_Deaths=Deaths-lag(Deaths), 
-               New_Deaths=ifelse(New_Deaths<0,0,New_Deaths),
-               New_Deaths_Average=rollmean(New_Deaths,k = 7,fill = NA, align = "right"),
-               New_Deaths_Per_Million=(New_Deaths/County_Population)*1000000,
-               New_Deaths_Per_Million_Average=(New_Deaths_Average/County_Population)*1000000) %>% 
-        ungroup() %>% 
-    #Keep only date of interest
-        filter(Date==as.Date(input$map_date)) %>% 
-    #Add ratio measures
-        mutate(Share_of_Pop=County_Population/State_Population*100,
-               Share_of_Cases=Cases/sum(Cases,na.rm = T)*100,
-               Share_of_Deaths=Deaths/sum(Deaths,na.rm = T)*100,
-               Case_Ratio=Share_of_Cases/Share_of_Pop,
-               Death_Ratio=Share_of_Deaths/Share_of_Pop,
-               Deaths_per_Case=Deaths/Cases*100) %>% 
-    #Make labels for tooltip
-        mutate(#Labels for cases: total, total per million, new, new per million, new on average, new on average per million
-            Cases_Label=glue("{prettyNum(round(Cases,0),big.mark = ',',big.interval = 3)} total cases were reported in {County} County as of {input$map_date}"),
-            Cases_Per_Million_Label=glue("{prettyNum(round(Cases_Per_Million,2),big.mark = ',',big.interval = 3)} total cases per million residents were reported in {County} County as of {input$map_date}"),
-            New_Cases_Label=glue("{prettyNum(round(New_Cases,0),big.mark = ',',big.interval = 3)} new cases were reported in {County} County on {input$map_date}"),
-            New_Cases_Average_Label=glue("{prettyNum(round(New_Cases_Average,2),big.mark = ',',big.interval = 3)} new cases were reported on average in {County} County for the week ending on {input$map_date}"),
-            New_Cases_Per_Million_Label=glue("{prettyNum(round(New_Cases_Per_Million,2),big.mark = ',',big.interval = 3)} new cases per million residents were reported in {County} County on {input$map_date}"),
-            New_Cases_Per_Million_Average_Label=glue("{prettyNum(round(New_Cases_Per_Million_Average,2),big.mark = ',',big.interval = 3)} new cases per million residents on average were reported \nin {County} County for the week ending on {input$map_date}"),
-            
-            #Labels for deaths: total, total per million, new, new per million, new on average, new on average per million
-            Deaths_Label=glue("{prettyNum(round(Deaths,0),big.mark = ',',big.interval = 3)} total deaths were reported in {County} County as of {input$map_date}"),
-            Deaths_Per_Million_Label=glue("{prettyNum(round(Deaths_Per_Million,2),big.mark = ',',big.interval = 3)} total deaths per million residents were reported in {County} County as of {input$map_date}"),
-            New_Deaths_Label=glue("{prettyNum(round(New_Deaths,0),big.mark = ',',big.interval = 3)} new deaths were reported in {County} County on {input$map_date}"),
-            New_Deaths_Average_Label=glue("{prettyNum(round(New_Deaths_Average,2),big.mark = ',',big.interval = 3)} new deaths were reported on average in {County} County for the week ending on {input$map_date}"),
-            New_Deaths_Per_Million_Label=glue("{prettyNum(round(New_Deaths_Per_Million,2),big.mark = ',',big.interval = 3)} new deaths per million residents were reported in {County} County on {input$map_date}"),
-            New_Deaths_Per_Million_Average_Label=glue("{prettyNum(round(New_Deaths_Per_Million_Average,2),big.mark = ',',big.interval = 3)} new deaths per million residents on average were reported \nin {County} County for the week ending on {input$map_date}"),
-            
-            #Labels for ratios
-            Case_Ratio_Label=glue("{prettyNum(round(Case_Ratio,2),big.mark = ',',big.interval = 3)} is the ratio of percent of cases reported in {County} County divided by the county's share of the state population"),
-            Death_Ratio_Label=glue("{prettyNum(round(Death_Ratio,2),big.mark = ',',big.interval = 3)} is the ratio of percent of deaths reported in {County} County divided by the county's share of the state population"),
-            Deaths_per_Case_Label=glue("{round(Deaths_per_Case,2)}% of cases resulted in death in {County} County as of {input$map_date}")
-        ))
+    US_Data <- reactive({
+        withProgress(message = "Getting your requested data", {
+                         incProgress(amount = 1/4, message = "Cleaning raw data")
+                         Data <- 
+                             US_Data_Raw %>% 
+                             #keep only selected state
+                             filter(state==input$statenameformap,
+                                    date>=input$map_date-7 & date<=input$map_date,
+                                    county!="Unknown",
+                                    !str_detect(county,"Aleutians West")) %>% # This filters the data to just be the seven days leading up to the date of interest, so we can get the 7 day rolling average
+                             #Add state names
+                             left_join(State_Names, by=c("state")) %>% 
+                             #Add state pops
+                             left_join(state_pop_clean,by="state") %>%
+                             #Add county pops
+                             left_join(county_pop_clean, by=c("state","county")) %>% 
+                             #Rename some columns
+                             rename("County"=county,"State"=state, 
+                                    "Date"=date, "Cases"=cases,"Deaths"=deaths) %>% 
+                             #Grab the county fips and add NYC pop
+                             mutate(county_fips=substr(fips,3,5),
+                                    County_Population=ifelse(County=="New York City", 8419000, County_Population))
+                         
+                         #Generate new variables
+                         incProgress(amount = 3/4, message = "Calculating statistics", 
+                                     detail = "This will take a little bit...")
+                         Data <- Data %>% 
+                             arrange(County,Date) %>% 
+                             group_by(fips) %>% 
+                             mutate(Cases_Per_Million=Cases/County_Population*1000000,
+                                    Deaths_Per_Million=Deaths/County_Population*1000000,
+                                    
+                                    New_Cases=Cases-lag(Cases), ##New cases is today's cases minus yesterdays
+                                    New_Cases=ifelse(New_Cases<0,0,New_Cases), # Set it to zero if it is negative
+                                    New_Cases_Average=rollmean(New_Cases,k = 7,fill = NA, align = "right"),
+                                    New_Cases_Per_Million=(New_Cases/County_Population)*1000000,
+                                    New_Cases_Per_Million_Average=(New_Cases_Average/County_Population)*1000000,
+                                    
+                                    New_Deaths=Deaths-lag(Deaths), 
+                                    New_Deaths=ifelse(New_Deaths<0,0,New_Deaths),
+                                    New_Deaths_Average=rollmean(New_Deaths,k = 7,fill = NA, align = "right"),
+                                    New_Deaths_Per_Million=(New_Deaths/County_Population)*1000000,
+                                    New_Deaths_Per_Million_Average=(New_Deaths_Average/County_Population)*1000000) %>% 
+                             ungroup() %>% 
+                             #Keep only date of interest
+                             filter(Date==as.Date(input$map_date)) %>% 
+                             #Add ratio measures
+                             mutate(Share_of_Pop=County_Population/State_Population*100,
+                                    Share_of_Cases=Cases/sum(Cases,na.rm = T)*100,
+                                    Share_of_Deaths=Deaths/sum(Deaths,na.rm = T)*100,
+                                    Case_Ratio=Share_of_Cases/Share_of_Pop,
+                                    Death_Ratio=Share_of_Deaths/Share_of_Pop,
+                                    Deaths_per_Case=Deaths/Cases*100) %>% 
+                             #Make labels for tooltip
+                             mutate(#Labels for cases: total, total per million, new, new per million, new on average, new on average per million
+                                 Cases_Label=glue("{prettyNum(round(Cases,0),big.mark = ',',big.interval = 3)} total cases were reported in {County} County as of {input$map_date}"),
+                                 Cases_Per_Million_Label=glue("{prettyNum(round(Cases_Per_Million,2),big.mark = ',',big.interval = 3)} total cases per million residents were reported in {County} County as of {input$map_date}"),
+                                 New_Cases_Label=glue("{prettyNum(round(New_Cases,0),big.mark = ',',big.interval = 3)} new cases were reported in {County} County on {input$map_date}"),
+                                 New_Cases_Average_Label=glue("{prettyNum(round(New_Cases_Average,2),big.mark = ',',big.interval = 3)} new cases were reported on average in {County} County for the week ending on {input$map_date}"),
+                                 New_Cases_Per_Million_Label=glue("{prettyNum(round(New_Cases_Per_Million,2),big.mark = ',',big.interval = 3)} new cases per million residents were reported in {County} County on {input$map_date}"),
+                                 New_Cases_Per_Million_Average_Label=glue("{prettyNum(round(New_Cases_Per_Million_Average,2),big.mark = ',',big.interval = 3)} new cases per million residents on average were reported \nin {County} County for the week ending on {input$map_date}"),
+                                 
+                                 #Labels for deaths: total, total per million, new, new per million, new on average, new on average per million
+                                 Deaths_Label=glue("{prettyNum(round(Deaths,0),big.mark = ',',big.interval = 3)} total deaths were reported in {County} County as of {input$map_date}"),
+                                 Deaths_Per_Million_Label=glue("{prettyNum(round(Deaths_Per_Million,2),big.mark = ',',big.interval = 3)} total deaths per million residents were reported in {County} County as of {input$map_date}"),
+                                 New_Deaths_Label=glue("{prettyNum(round(New_Deaths,0),big.mark = ',',big.interval = 3)} new deaths were reported in {County} County on {input$map_date}"),
+                                 New_Deaths_Average_Label=glue("{prettyNum(round(New_Deaths_Average,2),big.mark = ',',big.interval = 3)} new deaths were reported on average in {County} County for the week ending on {input$map_date}"),
+                                 New_Deaths_Per_Million_Label=glue("{prettyNum(round(New_Deaths_Per_Million,2),big.mark = ',',big.interval = 3)} new deaths per million residents were reported in {County} County on {input$map_date}"),
+                                 New_Deaths_Per_Million_Average_Label=glue("{prettyNum(round(New_Deaths_Per_Million_Average,2),big.mark = ',',big.interval = 3)} new deaths per million residents on average were reported \nin {County} County for the week ending on {input$map_date}"),
+                                 
+                                 #Labels for ratios
+                                 Case_Ratio_Label=glue("{prettyNum(round(Case_Ratio,2),big.mark = ',',big.interval = 3)} is the ratio of percent of cases reported in {County} County divided by the county's share of the state population"),
+                                 Death_Ratio_Label=glue("{prettyNum(round(Death_Ratio,2),big.mark = ',',big.interval = 3)} is the ratio of percent of deaths reported in {County} County divided by the county's share of the state population"),
+                                 Deaths_per_Case_Label=glue("{round(Deaths_per_Case,2)}% of cases resulted in death in {County} County as of {input$map_date}")
+                             )
+                     })
+    })
     
     #This disables the per million and rolling mean checkboxes if a relevant measure isn't selected.
     # So they are enabled if the user selects New Cases, but they are disabled if the user selects
