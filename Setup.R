b98bf52eb22d7d7c7f70348febf7b3482cfccf7f
jakepscott <jakepscott16@gmail.com>
2021-06-22 20:58:42 -0400

    Use data.table in setup for speed

--- Setup.R
+++ Setup.R
@@ -6,19 +6,22 @@ county_pop_clean <- read_rds("data/county_pop_clean.RDS")
 state_pop_clean <- read_rds("data/state_pop_clean.RDS")
 
 #Read in cases and deaths data from NYT
-US_Data_Raw <- vroom(url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"))
+US_Data_Raw <- fread(("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"))
+
+US_Data_Raw <- lazy_dt(US_Data_Raw)
 
 #Add state names and pop data to NYT Data
-US_Data <- left_join(US_Data_Raw,State_Names, by=c("state")) %>% 
-  left_join(state_pop_clean,by="state") %>% 
-  left_join(county_pop_clean, by=c("state","county")) %>%
+US_Data <- US_Data_Raw %>% 
+  left_join(lazy_dt(State_Names), by = c("state")) %>% 
+  left_join(lazy_dt(state_pop_clean), by = "state") %>% 
+  left_join(lazy_dt(county_pop_clean), by = c("state", "county")) %>%
   rename("County"=county,"State"=state, "Date"=date, "Cases"=cases,"Deaths"=deaths) %>% #Rename some columns
   #keep only states I can map
-  filter(State %in% State_Names$state | State=="District of Columbia") 
+  filter(State %in% State_Names$state | State=="District of Columbia")
 
 # Getting Grouped Data ----------------------------------------------------
 ##Grouping by Date and State to see cases per day by state
-US_Grouped <- US_Data %>%
+US_Grouped_dt <- US_Data %>%
   arrange(Date) %>%
   group_by(Date, State) %>%
   summarise(Cases=sum(Cases),
@@ -29,27 +32,28 @@ US_Grouped <- US_Data %>%
   mutate(cases_per_million=(Cases/Population)*1000000,
          Deaths_Per_Million=(Deaths/Population)*1000000)
 
+
 ##Getting a Column for Current Cases by State
-current_cases <- US_Grouped %>% 
+current_cases <- US_Grouped_dt %>% 
   group_by(State) %>%
   dplyr::summarise(current_cases=last(Cases)) 
 
 #Joining current cases with grouped data, adding per capita column
-US_Grouped <- left_join(US_Grouped,current_cases, by="State") %>%
+US_Grouped_dt <- left_join(US_Grouped_dt,current_cases, by="State") %>%
   group_by(State) %>%
   mutate(current_cases_per_million=(current_cases/Population)*1000000)#Making things in terms of 1 million residents
 
 ##Getting a Variable for Current Deaths by State
-Current_Deaths <- US_Grouped %>% 
+Current_Deaths <- US_Grouped_dt %>% 
   group_by(State) %>%
   dplyr::summarise(Current_Deaths=last(Deaths)) 
 
-US_Grouped <- left_join(US_Grouped,Current_Deaths, by="State") %>% 
+US_Grouped_dt <- left_join(US_Grouped_dt,Current_Deaths, by="State") %>% 
   group_by(State) %>% 
   mutate(Current_Deaths_Per_Million=(Current_Deaths/Population)*1000000)#Making things in terms of 1 million residents
 
 #Getting a column for new cases and deaths
-US_Grouped <- US_Grouped %>%
+US_Grouped_dt <- US_Grouped_dt %>%
   group_by(State) %>%
   mutate(New_Cases=Cases-lag(Cases),
          New_Deaths=Deaths-lag(Deaths)) %>%  ##New cases is today's cases minus yesterdays
@@ -62,4 +66,8 @@ US_Grouped <- US_Grouped %>%
          New_Deaths_Per_Million=(New_Deaths/Population)*1000000) 
 
 
-
+# Converting from data.table to tibble ------------------------------------
+US_Data <- US_Data %>% as_tibble() %>% mutate(Date=as.Date(Date))
+US_Grouped <- US_Grouped_dt %>% as_tibble() %>% mutate(Date=as.Date(Date))
+current_cases <- current_cases %>% as_tibble()
+Current_Deaths <- Current_Deaths %>% as_tibble()
